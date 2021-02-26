fd90f737a24aae67f8235175ec4acb9f43e81dd6
sakoda <sakoda@keio.jp>
2021-02-26 14:11:22 +0900

    add simple AutoEncoder

---
+++ sources/model.py
@@ -0,0 +1,14 @@
+import torch
+
+class AutoEncoder(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(AutoEncoder, self).__init__()    
+        self.linear0 = torch.nn.Linear(input_size, hidden_size)
+        self.linear1 = torch.nn.Linear(hidden_size, input_size)
+
+    def forward(self, x):
+        x = self.linear0(x)
+        x = torch.nn.functional.relu(x)
+        x = self.linear1(x)
+        x = torch.nn.functional.sigmoid(x)
+        return x
