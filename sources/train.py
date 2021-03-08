922934a6c5985b6c66c9e58d96fb45d7b3c4dc15
sakoda <sakoda@keio.jp>
2021-03-08 17:13:58 +0900

    Removed upside down data expansion and added Random Crop

--- sources/train.py
+++ sources/train.py
@@ -70,8 +70,7 @@ def main():
         [torchvision.transforms.ToTensor(),
          torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
          torchvision.transforms.RandomHorizontalFlip(),
-         torchvision.transforms.RandomVerticalFlip(),
-         ])
+         torchvision.transforms.RandomCrop(32, padding=4)])
     transform_normal = torchvision.transforms.Compose(
         [torchvision.transforms.ToTensor(),
          torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
